f3fb92160b0074b22c7fb7f991e684a24236a446
SerhiiCho <serhiicho@protonmail.com>
2023-03-02 10:26:07 +0200

    Added toast

--- wp/src/wp-content/plugins/wp-pager/src/album/main.ts
+++ wp/src/wp-content/plugins/wp-pager/src/album/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from '@album/App.vue'
 import store from './store'
+import 'toastify-js/src/toastify.css'
 
 const app = createApp(App)
 
---
+++ wp/src/wp-content/plugins/wp-pager/src/shared/modules/showToast.ts
@@ -0,0 +1,39 @@
+import toast from 'toastify-js'
+
+type ShowToastParams = {
+    text: string
+    success?: boolean
+    url?: string
+    duration?: number
+}
+
+export default (params: ShowToastParams) => {
+    if (typeof params.success === 'undefined')
+        params.success = true
+
+    const successIcon = pager.rootUrl + '/assets/img/success.svg'
+    const errorIcon = pager.rootUrl + '/assets/img/error.svg'
+
+    const config: toast.Options = {
+        text: params.text,
+        destination: params.url || 'javascript:',
+        gravity: 'bottom',
+        position: 'right',
+        backgroundColor: 'white',
+        close: true,
+        avatar: params.success ? successIcon : errorIcon,
+        duration: params.duration || 2000,
+        style: {
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            gap: '10px',
+            borderRadius: '8px',
+            color: '#777',
+            padding: '15px',
+            boxShadow: '0 0 17px rgba(0, 0, 0, .3)',
+        },
+    }
+
+    toast(config).showToast()
+}
