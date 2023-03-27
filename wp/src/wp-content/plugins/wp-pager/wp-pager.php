1c75b35b27398d0b61c922b2b46ac0d381215dad
SerhiiCho <serhiicho@protonmail.com>
2023-03-27 12:23:22 +0300

    Changed version

--- wp/src/wp-content/plugins/wp-pager/wp-pager.php
+++ wp/src/wp-content/plugins/wp-pager/wp-pager.php
@@ -7,7 +7,7 @@ Plugin Name: WP Pager
 Author: Serhii Cho
 Author URI: https://serhii.io/about-me
 Description: WP Pager is a WordPress plugin for displaying images in a form of a restaurant menu. The main advantage of such plugin is that you can turn pages of the menu by swiping on mobile and desktop devices.
-Version: 0.3
+Version: 0.2.0
 License: MIT
 License URI: https://github.com/SerhiiCho/wp-pager/blob/main/LICENCE
 Text Domain: pager
@@ -17,7 +17,7 @@ Tags: menu, album, images, files, gallery, restaurant, food, food menu, restaura
 use WpPager\Hook;
 
 defined('ABSPATH') || exit('ABSPATH is not defined');
-define('PAGER_VERSION', '0.3');
+define('PAGER_VERSION', '0.2.0');
 define('PAGER_PATH', plugin_dir_path(__FILE__));
 define('PAGER_URL', plugin_dir_url(__FILE__));
 define('PAGER_FILES_URL', PAGER_PATH . 'storage/files.json');
