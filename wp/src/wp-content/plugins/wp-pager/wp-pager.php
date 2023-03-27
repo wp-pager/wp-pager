179cfca8df0a7677922afcd547d7ea4eb80474ff
SerhiiCho <serhiicho@protonmail.com>
2023-03-27 13:58:47 +0300

    Changed wp-pager.php

--- wp/src/wp-content/plugins/wp-pager/wp-pager.php
+++ wp/src/wp-content/plugins/wp-pager/wp-pager.php
@@ -7,8 +7,10 @@ Plugin Name: WP Pager
 Author: Serhii Cho
 Author URI: https://serhii.io/about-me
 Description: WP Pager is a WordPress plugin for displaying images in a form of a restaurant menu.
-o
-Version: 0.2.1
+Version: 0.3.0
+Tested up to: 8.0
+Requires PHP: 8.1
+Donate link: https://www.buymeacoffee.com/serhiicho
 License: MIT
 License URI: https://github.com/SerhiiCho/wp-pager/blob/main/LICENCE
 Text Domain: pager
@@ -21,11 +23,6 @@ defined('ABSPATH') || exit('ABSPATH is not defined');
 define('PAGER_VERSION', '0.2.1');
 define('PAGER_PATH', plugin_dir_path(__FILE__));
 define('PAGER_URL', plugin_dir_url(__FILE__));
-define('PAGER_STORAGE_PATH', PAGER_PATH . 'storage/');
-define('PAGER_FILES_URL', PAGER_STORAGE_PATH . 'files.json');
-define('PAGER_SETTINGS_URL', PAGER_STORAGE_PATH . 'settings.json');
-define('PAGER_FILES_DIR', PAGER_STORAGE_PATH . 'files');
-define('PAGER_FILES_DIR_URL', PAGER_URL . 'storage/files');
 
 error_reporting(E_ERROR | E_PARSE);
 
