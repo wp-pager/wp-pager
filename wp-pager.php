911a8631ce7263bfcfd6c72646b91ad8f8f6721a
SerhiiCho <serhiicho@protonmail.com>
2023-03-28 10:21:29 +0300

    Changed version

--- wp-pager.php
+++ wp-pager.php
@@ -7,7 +7,7 @@ Plugin Name: WP Pager
 Author: Serhii Cho
 Author URI: https://serhii.io/about-me
 Description: WP Pager is a WordPress plugin for displaying images in a form of a restaurant menu.
-Version: 0.4
+Version: 0.3
 License: MIT
 License URI: https://github.com/SerhiiCho/wp-pager/blob/main/LICENCE
 Text Domain: pager
