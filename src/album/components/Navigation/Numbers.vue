b3b8d82cce5cb71901f03acbd9e2bb45f97b4b68
SerhiiCho <serhiicho@protonmail.com>
2023-03-29 14:08:00 +0300

    Added useAlbumNumbers composable

--- src/album/components/Navigation/Numbers.vue
+++ src/album/components/Navigation/Numbers.vue
@@ -1,63 +1,10 @@
 <script setup lang="ts">
-import type { ImageFile } from '@shared/types'
-import { useStore } from 'vuex'
-import { computed } from 'vue'
 import NestedNumber from '@album/components/Navigation/NestedNumber.vue'
 import Number from '@album/components/Navigation/Number.vue'
 import Line from '@album/components/Navigation/Line.vue'
+import useAlbumNumbers from '@album/composables/useAlbumNumbers'
 
-const store = useStore()
-const files = computed<ImageFile[]>(() => store.getters['files/files'])
-const prevPageNum = computed<number>(() => store.getters['files/prevPageNum'])
-
-type GroupedFile = {
-    title: string | number
-    visible: boolean
-    files: ImageFile[]
-}
-
-const groupedFiles = computed<GroupedFile[]>(() => {
-    if (!files.value) {
-        return []
-    }
-
-    const result: GroupedFile[] = files.value.reduce((groups: GroupedFile[], file) => {
-        const matchedGroup = groups.find((group) => group.title === file.title || group.title === file.page)
-
-        if (!matchedGroup) {
-            const newGroup = {
-                title: file.title || file.page,
-                visible: false,
-                files: [file]
-            }
-
-            groups.push(newGroup)
-
-            return groups
-        }
-
-        matchedGroup.files.push(file)
-
-        return groups
-    }, [])
-
-    result[0].visible = true
-
-    return result
-})
-
-async function pageChosenHandler(pageNum: number) {
-    if (pageNum > prevPageNum.value) {
-        await store.dispatch('swipe/setTouchStart', 1)
-        await store.dispatch('swipe/setTouchEnd', 0)
-    } else if (pageNum < prevPageNum.value) {
-        await store.dispatch('swipe/setTouchStart', 0)
-        await store.dispatch('swipe/setTouchEnd', 1)
-    }
-
-    await store.dispatch('files/setPrevPageNum', pageNum)
-    await store.dispatch('files/setCurrPageNum', pageNum)
-}
+const { groupedFiles, pageChosenHandler } = useAlbumNumbers()
 </script>
 
 <template>
---
+++ src/album/composables/useAlbumNumbers.ts
@@ -0,0 +1,63 @@
+import type { ImageFile } from '@shared/types'
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+
+export default () => {
+    const store = useStore()
+    const files = computed<ImageFile[]>(() => store.getters['files/files'])
+    const prevPageNum = computed<number>(() => store.getters['files/prevPageNum'])
+
+    type GroupedFile = {
+        title: string | number
+        visible: boolean
+        files: ImageFile[]
+    }
+
+    const groupedFiles = computed<GroupedFile[]>(() => {
+        if (!files.value) {
+            return []
+        }
+
+        const result: GroupedFile[] = files.value.reduce((groups: GroupedFile[], file) => {
+            const matchedGroup = groups.find((group) => group.title === file.title || group.title === file.page)
+
+            if (!matchedGroup) {
+                const newGroup = {
+                    title: file.title || file.page,
+                    visible: false,
+                    files: [file]
+                }
+
+                groups.push(newGroup)
+
+                return groups
+            }
+
+            matchedGroup.files.push(file)
+
+            return groups
+        }, [])
+
+        result[0].visible = true
+
+        return result
+    })
+
+    async function pageChosenHandler(pageNum: number) {
+        if (pageNum > prevPageNum.value) {
+            await store.dispatch('swipe/setTouchStart', 1)
+            await store.dispatch('swipe/setTouchEnd', 0)
+        } else if (pageNum < prevPageNum.value) {
+            await store.dispatch('swipe/setTouchStart', 0)
+            await store.dispatch('swipe/setTouchEnd', 1)
+        }
+
+        await store.dispatch('files/setPrevPageNum', pageNum)
+        await store.dispatch('files/setCurrPageNum', pageNum)
+    }
+
+    return {
+        groupedFiles,
+        pageChosenHandler,
+    }
+}
